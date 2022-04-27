25760c1837e7f182537bcefc393b55289ae22367
adamekissa <adamekissa@gmail.com>
2022-04-27 21:54:18 +0100

    Add project files.

---
+++ AmjConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmjConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO AMJ CONSOLE APPLICATION");
+            Console.ReadKey();
+        }
+    }
+}
